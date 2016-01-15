2a47d515a78c532f359c3d45ab6d93169e7b5785
Charlie Conroy <conroy.charlie@gmail.com>
2016-01-14 23:24:52 -0500

    changed re-initialization to a ball around min(chi2)

--- src/pixcmd_vars.f90
+++ src/pixcmd_vars.f90
@@ -34,7 +34,7 @@ MODULE PIXCMD_VARS
   !if set, include observational errors
   INTEGER, PARAMETER :: incl_obs_err=1
   !if >0, then set the random seed to fix_seed
-  INTEGER :: fix_seed=9000
+  INTEGER :: fix_seed=3300
 
   !variables for model image and CMD Hess diagram
   INTEGER, PARAMETER  :: nx=121,ny=351,nfil=2
