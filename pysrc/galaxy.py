f18dca4d0a798080caebfc3c5b979329b42d08c6
Ben Cook <bcook@cfa.harvard.edu>
2017-04-14 12:20:40 -0400

    created mechanism for initializing with setup_files

--- pysrc/galaxy.py
+++ pysrc/galaxy.py
@@ -9,7 +9,9 @@ class Galaxy_Model:
 
     age_arr = np.array([6.5, 7.5, 8.25, 8.75, 9.25, 9.75, 10.1])
     _num_ages = len(age_arr)
-    _num_params = _num_ages + 2
+
+    _param_names = ['logz', 'logdust', 'logSFH0', 'logSFH1', 'logSFH2', 'logSFH3', 'logSFH4', 'logSFH5', 'logSFH6']
+    _num_params = len(_param_names)
     
     def __init__(self, gal_params):
         """
@@ -26,9 +28,10 @@ class Galaxy_Model:
         self.dust = 10.**gal_params[1]
         self.SFH = 10.**gal_params[2:]
         self.Npix = np.sum(self.SFH)
-        
+
 class Galaxy_SSP:
-    _num_params = 4
+    _param_names = ['logz', 'logdust', 'logNpix', 'logage']
+    _num_params = len(_param_names)
 
     def __init__(self, gal_params):
         """
--- pysrc/pcmd_fitter.py
+++ pysrc/pcmd_fitter.py
@@ -10,8 +10,7 @@ import fit_model
 import utils
 import gpu_utils
 import pandas as pd
-import os
-import sys, getopt
+import sys
 import multiprocessing
 import emcee
 import importlib
@@ -20,6 +19,7 @@ if __name__ == "__main__":
 
     #Setup from an external file
     setup_mod = sys.argv[1].strip('.py')
+    print('Loading Setup File: %s.py'%setup_mod)
     setup = importlib.import_module(setup_mod)
 
     N_scale = setup.N_scale
@@ -27,8 +27,38 @@ if __name__ == "__main__":
     N_burn = setup.N_burn
     N_sample = setup.N_sample
     N_threads = setup.N_threads
-    pool = None
-    gpu = setup.gpu
-    force_gpu = setup.force_gpu
+    pool = setup.pool 
+    gpu = setup.use_gpu
 
     fixed_seed = setup.fixed_seed
+
+    filters = setup.filters
+    iso_model = setup.iso_model
+
+    data_is_mock = setup.data_is_mock
+    data_pcmd = setup.pcmd_data
+    if data_mocked:
+        galaxy_mock = setup.mock_galaxy
+        N_mock = setup.N_mock
+
+    model_class = setup.model_class
+    param_names = model_class._param_names
+    N_params = len(param_names)
+    p0 = setup.p0
+    
+    chain_file = setup.chain_file
+
+    print('Running emcee')
+    sampler = fit_model.sample_post(data_pcmd, filters, N_scale, N_walkers, N_burn, N_sample, fixed_seed=fixed_seed,
+                                    gal_class=model_class, gpu=gpu, pool=pool, p0=p0)
+
+    print('emcee complete, saving results')
+    #Save results of the chain
+    chain_df = pd.DataFrame()
+    for d in range(N_params):
+        chain_df[params[d]] = sampler.flatchain[:,d]
+    chain_df['lnprob'] = sampler.flatlnprobability
+    chain_df['walker'] = np.repeat(np.arange(N_walkers), N_sample)
+    chain_df['accept_frac'] = np.repeat(sampler.acceptance_fraction, N_sample)
+
+    chain_df.to_csv(chain_file, index=False, float_format='%.4f', compression='gzip', 
