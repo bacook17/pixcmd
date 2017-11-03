7ac2fe1f4f756bff0fbe2684fce36834234f7d5a
Ben Cook <bcook@cfa.harvard.edu>
2017-11-03 09:15:52 -0400

    want to run first dynamic test

--- pcmdpy/fit_model.py
+++ pcmdpy/fit_model.py
@@ -241,7 +241,7 @@ def nested_integrate(pcmd, filters, im_scale, N_points, method='multi', max_call
         print('-Running dynesty dynamic sampler')
         sampler.run_nested(nlive_init=N_points, maxcall=max_call, nlive_batch=N_batch,
                            wt_kwargs={'pfrac':1.0}, save_proposals=False,
-                           print_progress=True, dlogz_init=dlogz)
+                           print_progress=True, print_to_stderr=False, dlogz_init=dlogz)
     else:
         sampler = dynesty.NestedSampler(this_lnlike, this_pri_transform, ndim=n_dim,
                                         bound=method, sample='unif', nlive=N_points,
