0104f71778e050fb8a14fb2c159f07190498254a
Ben Cook <bcook@cfa.harvard.edu>
2018-04-18 16:46:20 -0400

    running mock_tau_dmod and SFH6 and 6

--- scripts_py/setup_files/mock_SFH5.py
+++ scripts_py/setup_files/mock_SFH5.py
@@ -202,9 +202,9 @@ prior_bounds = {}
 prior_bounds['feh_bounds'] = [z_bound]
 prior_bounds['dust_bounds'] = [dust_med_bound]
 prior_bounds['age_bounds'] = SFH_bounds
-prior_bounds['dmod_bound'] = dmod_bound
+prior_bounds['dmod_bounds'] = dmod_bound
 
-params['prior'] = params['gal_class'].get_flat_prior(**prior_bounds)
+params['prior'] = params['gal_model'].get_flat_prior(**prior_bounds)
 
 ###############################################
 # DATA / MOCK SETTINGS
