4296cd0781ec35cd40a9e1833cbf39a280f80d87
Ben Cook <bcook@cfa.harvard.edu>
2019-02-25 08:40:12 -0500

    running mock SFH runs

--- scripts_py/setup_files/mocks_paper1/mock_49.py
+++ scripts_py/setup_files/mocks_paper1/mock_49.py
@@ -77,7 +77,7 @@ sampler_params['pool'] = pool
 params['dynamic'] = DYNAMIC = True
 
 # The number of dynesty live points
-_nlive = 500
+_nlive = 1000
 if DYNAMIC:
     run_params['nlive_init'] = _nlive
 else:
@@ -108,7 +108,8 @@ sampler_params['first_update'] = {'min_eff': 30.}
 # DYNESTY RUN_NESTED SETTINGS
 
 # The number of max calls for dynesty
-run_params['maxcall'] = 250000
+run_params['maxcall'] = 200000
+params['continue_run'] = False
 
 # The error tolerance for dynesty stopping criterion
 _dlogz = 0.5
