8a7b35458624732ec5b5ee7c0d06a6afa8feb8de
Ben Cook <bcook@cfa.harvard.edu>
2019-04-08 21:13:10 -0400

    blah

--- paper2/setup_files/results.py
+++ paper2/setup_files/results.py
@@ -1,5 +1,4 @@
-__all__ = ['models', 'results', 'pcmds', 'data', 'filters', 'iso_models',
-           'drivers']
+__all__ = ['models', 'results', 'pcmds', 'data']
 
 try:
     import pcmdpy_gpu as ppy
@@ -40,6 +39,27 @@ model_ssp = ppy.galaxy.CustomGalaxy(
     ppy.sfhmodels.SSPModel(),
     ppy.distancemodels.VariableDistance()
 )
+model_ssp_mdf = ppy.galaxy.CustomGalaxy(
+    ppy.metalmodels.FixedWidthNormMDF(0.2),
+    ppy.dustmodels.SingleDust(),
+    ppy.sfhmodels.SSPModel(),
+    ppy.distancemodels.VariableDistance()
+)
+model_ssp_fixed = ppy.galaxy.CustomGalaxy(
+    ppy.metalmodels.SingleFeH(),
+    ppy.dustmodels.SingleDust(),
+    ppy.sfhmodels.SSPModel(),
+    ppy.distancemodels.FixedDistance()
+)
+custom_sfh = ppy.sfhmodels.NonParam()
+custom_sfh.update_sfh_edges(np.array([9.5, 9.75, 10.0, 10.2]))
+custom_sfh.update_edges(np.arange(9.0, 10.3, 0.1))
+model_df2_nonparam = ppy.galaxy.CustomGalaxy(
+    ppy.metalmodels.SingleFeH(),
+    ppy.dustmodels.SingleDust(),
+    custom_sfh,
+    ppy.distancemodels.VariableDistance()
+)
 
 
 def add_set(galaxy, mnum, region, key, model=model_nonparam,
@@ -59,7 +79,7 @@ def add_set(galaxy, mnum, region, key, model=model_nonparam,
     except:
         pass
 
-
+    
 # M87
 print('M87')
 add_set('M87', 3, 44, 'M87_m3', colors='I_VI')
@@ -72,6 +92,14 @@ add_set('M87', 6, 264, 'M87_m6', colors='I_VI')
 add_set('M87', 7, 104, 'M87_m7', model=model_fixeddist, colors='I_VI')
 add_set('M87', 8, 104, 'M87_m8', model=model_tau, colors='I_VI')
 add_set('M87', 9, 104, 'M87_m9', model=model_ssp, colors='I_VI')
+add_set('M87', 10, 104, 'M87_m10', model=model_ssp, colors='I_VI')
+add_set('M87', 11, 104, 'M87_m11', model=model_ssp, colors='I_VI')
+add_set('M87', 12, 104, 'M87_m12', model=model_ssp_fixed, colors='I_VI')
+add_set('M87', 13, 104, 'M87_m13', model=model_ssp, colors='I_VI')
+add_set('M87', 14, 104, 'M87_m14', model=model_ssp, colors='I_VI')
+add_set('M87', 15, 104, 'M87_m15', model=model_ssp_mdf, colors='I_VI')
+add_set('M87', 16, 104, 'M87_m16', model=model_ssp, colors='I_VI')
+add_set('M87', 17, 104, 'M87_m17', model=model_ssp, colors='I_VI')
 
 # M49
 print('M49')
@@ -85,6 +113,7 @@ add_set('M49', 6, 256, 'M49_m6')
 add_set('M49', 7, 100, 'M49_m7', model=model_fixeddist)
 add_set('M49', 8, 100, 'M49_m8', model=model_tau)
 add_set('M49', 9, 100, 'M49_m9', model=model_ssp)
+add_set('M49', 10, 100, 'M49_m10', model=model_ssp)
 
 # NGC 3377
 print('NGC3377')
@@ -98,6 +127,7 @@ add_set('NGC3377', 6, 241, 'NGC3377_m6')
 add_set('NGC3377', 7, 97, 'NGC3377_m7', model=model_fixeddist)
 add_set('NGC3377', 8, 97, 'NGC3377_m8', model=model_tau)
 add_set('NGC3377', 9, 97, 'NGC3377_m9', model=model_ssp)
+add_set('NGC3377', 10, 97, 'NGC3377_m10', model=model_ssp)
 
 # NGC 4993
 print('NGC4993')
@@ -114,28 +144,19 @@ add_set('NGC4993', 9, 83, 'NGC4993_m9', model=model_ssp)
 
 # DF2
 print('DF2')
-df2_res = results_dir + 'DF2_m1.csv'
-df2_data = data_dir + 'DF2/pcmds/DF2_I_VI_1.pcmd'
-results['DF2'] = ppy.results.ResultsPlotter(
-    df2_res, run_name='DF2',
-    gal_model=model_ssp.copy(), model_is_truth=False)
-data['DF2'] = np.loadtxt(df2_data, unpack=True)
-try:
-    pcmds['DF2'] = np.loadtxt(df2_res.replace('.csv', '.pcmd'), unpack=True)
-except:
-    pass
-
-
-filters = {}
-filters['M87'] = ppy.instrument.default_m87_filters()
-filters['M49'] = ppy.instrument.default_m49_filters()
-filters['NGC3377'] = ppy.instrument.default_ngc3377_filters()
-filters['NGC4993'] = ppy.instrument.default_ngc4993_filters()
-filters['DF2'] = ppy.instrument.default_df2_filters()
-
-print('Loading Isochrone Models')
-iso_models = {}
-drivers = {}
-for k, f in filters.items():
-    iso_models[k] = ppy.isochrones.Isochrone_Model(f)
-    drivers[k] = ppy.driver.Driver(iso_models[k])
+for i in range(1, 5):
+    df2_res = results_dir + f'DF2_m{i}.csv'
+    df2_live = df2_res.replace('.csv', '_live.csv')
+    df2_data = data_dir + 'DF2/pcmds/DF2_I_VI_1.pcmd'
+    if i in [2, 4]:
+        model = model_df2_nonparam.copy()
+    else:
+        model = model_ssp.copy()
+    results[f'DF2_m{i}'] = ppy.results.ResultsPlotter(
+        df2_res, live_file=df2_live, run_name=f'DF2, model {i}',
+        gal_model=model, model_is_truth=False)
+    data[f'DF2_m{i}'] = np.loadtxt(df2_data, unpack=True)
+    try:
+        pcmds[f'DF2_m{i}'] = np.loadtxt(df2_res.replace('.csv', '.pcmd'), unpack=True)
+    except:
+        pass
