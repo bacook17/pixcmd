e3f7307b611272c89557b32f5794ef0506d56383
Ben Cook <bcook@cfa.harvard.edu>
2019-04-12 16:34:52 -0400

    Working on widths

--- paper2/drivers.py
+++ paper2/drivers.py
@@ -5,14 +5,26 @@ except:
 
 filters = {}
 filters['M87'] = ppy.instrument.default_m87_filters()
+filters['M87v2'] = ppy.instrument.m87_filters_v2()
 filters['M49'] = ppy.instrument.default_m49_filters()
 filters['NGC3377'] = ppy.instrument.default_ngc3377_filters()
 filters['NGC4993'] = ppy.instrument.default_ngc4993_filters()
 filters['DF2'] = ppy.instrument.default_df2_filters()
+filters['M31'] = ppy.instrument.m31_winter_filters()
+filters['M51'] = ppy.instrument.default_m51_filters()
 
 print('Loading Isochrone Models')
-iso_models = {}
+iso_models = {
+    'M87': ppy.isochrones.Isochrone_Model(filters['M87']),  # 814, 606
+    'NGC3377': ppy.isochrones.Isochrone_Model(filters['NGC3377']),  # 850, 475
+    'M51': ppy.isochrones.Isochrone_Model(filters['M51']),  # 814, 435
+    'M31': ppy.isochrones.Isochrone_Model(filters['M31']),  # 814, 475
+}
+iso_models['M87v2'] = iso_models['M31']
+iso_models['NGC4993'] = iso_models['M49'] = iso_models['NGC3377']
+iso_models['DF2'] = iso_models['M87']
 drivers = {}
-for k, f in filters.items():
-    iso_models[k] = ppy.isochrones.Isochrone_Model(f)
-    drivers[k] = ppy.driver.Driver(iso_models[k])
+for k, i in iso_models.items():
+    drivers[k] = ppy.driver.Driver(i)
+    drivers[k].filters = filters[k]
+
--- paper2/results.py
+++ paper2/results.py
@@ -24,10 +24,12 @@ dmods = {
     'NGC3377': 30.18,
     'NGC4993': 33.05,
     'M87': 31.02,
+    'M87v2': 31.02,
     'M49': 31.0,
     'DF2': 31.505,
+    'M31': 24.42,
+    'M51': 29.67
 }
-
 
 base_models = {}
 base_models[1] = ppy.galaxy.CustomGalaxy(
@@ -64,7 +66,8 @@ base_models[5] = ppy.galaxy.CustomGalaxy(
 
 def add_set(galaxy, mnum, region, key,
             colors='z_gz'):
-    data_file = data_dir + f'{galaxy.lower()}/pcmds/{galaxy}_{colors}_{region}.pcmd'
+    g_orig = galaxy.replace('v2', '')
+    data_file = data_dir + f'{galaxy.lower()}/pcmds/{g_orig}_{colors}_{region}.pcmd'
     res_file = results_dir + f'{galaxy}_r{region}_m{mnum}.csv'
     live_file = res_file.replace('.csv', '_live.csv')
     pcmd_file = res_file.replace('.csv', '.pcmd')
@@ -73,13 +76,37 @@ def add_set(galaxy, mnum, region, key,
     results[key] = ppy.results.ResultsPlotter(
         res_file, live_file=live_file, dmod_true=dmods[galaxy],
         gal_model=models[key], model_is_truth=False)
-    data[key] = np.loadtxt(data_file, unpack=True)
-    radii_am[key] = df_radii[galaxy][region] * 0.05 / 60.
+    if key.replace(f'_m{mnum}', '_m1') in data:
+        data[key] = data[key.replace(f'_m{mnum}', '_m1')]
+    else:
+        data[key] = np.loadtxt(data_file, unpack=True)
+    radii_am[key] = df_radii[g_orig][region] * 0.05 / 60.
     radii_kpc[key] = radii_am[key] * (np.pi/(180.*60.)) * 1e3 * ppy.distancemodels.dmod_to_mpc(dmods[galaxy])
     try:
         pcmds[key] = np.loadtxt(pcmd_file, unpack=True)
     except:
         pass
+
+    
+def add_set_v2(galaxy, mnum, region, key, data_name, model=None):
+    data_file = data_dir + f'{galaxy.lower()}/pcmds/{data_name}.pcmd'
+    res_file = results_dir + f'{galaxy}_{region}_m{mnum}.csv'
+    live_file = res_file.replace('.csv', '_live.csv')
+    pcmd_file = res_file.replace('.csv', '.pcmd')
+    regions[key] = region
+    models[key] = model or base_models[mnum].copy()
+    results[key] = ppy.results.ResultsPlotter(
+        res_file, live_file=live_file, dmod_true=dmods[galaxy],
+        gal_model=models[key], model_is_truth=False)
+    if key.replace(f'_m{mnum}', '_m1') in data:
+        data[key] = data[key.replace(f'_m{mnum}', '_m1')]
+    else:
+        data[key] = np.loadtxt(data_file, unpack=True)
+    try:
+        pcmds[key] = np.loadtxt(pcmd_file, unpack=True)
+    except:
+        pass
+
 
 # M87
 print('M87')
@@ -87,6 +114,12 @@ for m in range(1, 6):
     add_set('M87', m, 204, f'M87_a1_m{m}', colors='I_VI')
     add_set('M87', m, 128, f'M87_b1_m{m}', colors='I_VI')
     add_set('M87', m, 44, f'M87_c1_m{m}', colors='I_VI')
+
+print('M87v2')
+for m in range(1, 6):
+    add_set('M87v2', m, 204, f'M87v2_a1_m{m}', colors='I_gI')
+    add_set('M87v2', m, 128, f'M87v2_b1_m{m}', colors='I_gI')
+    add_set('M87v2', m, 44, f'M87v2_c1_m{m}', colors='I_gI')
 
 print('M49')
 for m in range(1, 6):
@@ -99,3 +132,49 @@ for m in range(1, 6):
     add_set('NGC3377', m, 173, f'NGC3377_a1_m{m}')
     add_set('NGC3377', m, 97, f'NGC3377_b1_m{m}')
     add_set('NGC3377', m, 41, f'NGC3377_c1_m{m}')
+
+print('NGC4993')
+for m in range(1, 6):
+    add_set('NGC4993', m, 203, f'NGC4993_a1_m{m}')
+    add_set('NGC4993', m, 143, f'NGC4993_b1_m{m}')
+    # add_set('NGC4993', m, 83, f'NGC4993_c1_m{m}')
+    
+print('M31')
+m31_regions = {
+    'a': 'summer_r1',
+    'b': 'winter_r9',
+    'c': 'winter_r1',
+    'd': 'winter_r2',
+    'e': 'winter_r10'
+}
+m31_rpix = {
+    'a': 6250,
+    'b': 3550,
+    'c': 2050,
+    'd': 1350,
+    'e': 700
+}
+for m in range(1, 6):
+    for i, r in enumerate(['a', 'b', 'c', 'd', 'e']):
+        k = f'M31_{r}_m{m}'
+        add_set_v2('M31', m, r, f'M31_{r}_m{m}', 'm31_'+m31_regions[r])
+        radii_am[k] = m31_rpix[r] * 0.05 / 60.
+        radii_kpc[k] = radii_am[k] * (np.pi/(180.*60.)) * 1e3 * ppy.distancemodels.dmod_to_mpc(dmods['M31'])
+
+print('M51')
+m51_rpix = {
+    'a': 1745,
+    'b': 2776,
+    'c': 2471,
+    'd': 2709,
+    'e': 678
+}
+for m in range(1, 5):
+    model = base_models[m].copy()
+    model.dust_model = ppy.dustmodels.FixedWidthLogNormDust(0.1)
+    for i, r in enumerate(['a', 'b', 'c', 'd', 'e']):
+        k = f'M51_{r}_m{m}'
+        add_set_v2('M51', m, r, f'M51_{r}_m{m}', f'M51_I_BI_{i+1}',
+                   model=model)
+        radii_am[k] = m51_rpix[r] * 0.05 / 60.
+        radii_kpc[k] = radii_am[k] * (np.pi/(180.*60.)) * 1e3 * ppy.distancemodels.dmod_to_mpc(dmods['M51'])
