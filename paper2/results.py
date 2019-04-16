4bb03a82f6839ba1ad8cdf96814de66f49992227
Ben Cook <bcook@cfa.harvard.edu>
2019-04-16 17:19:05 -0400

    Ready to process final plots

--- paper2/results.py
+++ paper2/results.py
@@ -63,6 +63,7 @@ base_models[5] = ppy.galaxy.CustomGalaxy(
     ppy.distancemodels.VariableDistance()
 )
 base_models[6] = base_models[1].copy()
+base_models[7] = base_models[5].copy()
 
 
 def add_set(galaxy, mnum, region, key,
@@ -79,7 +80,7 @@ def add_set(galaxy, mnum, region, key,
         gal_model=models[key], model_is_truth=False)
     if key.replace(f'_m{mnum}', '_m1') in data:
         data[key] = data[key.replace(f'_m{mnum}', '_m1')]
-    else:
+    elif key not in data:
         data[key] = np.loadtxt(data_file, unpack=True)
     radii_am[key] = df_radii[g_orig][region] * 0.05 / 60.
     radii_kpc[key] = radii_am[key] * (np.pi/(180.*60.)) * 1e3 * ppy.distancemodels.dmod_to_mpc(dmods[galaxy])
@@ -109,102 +110,127 @@ def add_set_v2(galaxy, mnum, region, key, data_name, model=None):
         pass
 
 
-# M87
-print('M87')
-for m in range(1, 6):
-    add_set('M87', m, 204, f'M87_a1_m{m}', colors='I_VI')
-    add_set('M87', m, 128, f'M87_b1_m{m}', colors='I_VI')
-    add_set('M87', m, 44, f'M87_c1_m{m}', colors='I_VI')
-for i, r in enumerate([204, 201, 202, 203]):
-    add_set('M87', 6, r, f'M87_a1_m6', colors='I_VI')
-for i, r in enumerate([128, 125, 126, 127]):
-    add_set('M87', 6, r, f'M87_b1_m6', colors='I_VI')
-for i, r in enumerate([44, 41, 42, 43]):
-    add_set('M87', 6, r, f'M87_c1_m6', colors='I_VI')
+def load_M87():
+    # M87
+    print('M87')
+    for m in range(1, 6):
+        add_set('M87', m, 204, f'M87_a1_m{m}', colors='I_VI')
+        add_set('M87', m, 128, f'M87_b1_m{m}', colors='I_VI')
+        add_set('M87', m, 44, f'M87_c1_m{m}', colors='I_VI')
+    for i, r in enumerate([204, 201, 202, 203]):
+        add_set('M87', 6, r, f'M87_a1_m6', colors='I_VI')
+    for i, r in enumerate([128, 125, 126, 127]):
+        add_set('M87', 6, r, f'M87_b1_m6', colors='I_VI')
+    for i, r in enumerate([44, 41, 42, 43]):
+        add_set('M87', 6, r, f'M87_c1_m6', colors='I_VI')
 
-    
-print('M87v2')
-for m in range(1, 6):
-    add_set('M87v2', m, 204, f'M87v2_a1_m{m}', colors='I_gI')
-    add_set('M87v2', m, 128, f'M87v2_b1_m{m}', colors='I_gI')
-    add_set('M87v2', m, 44, f'M87v2_c1_m{m}', colors='I_gI')
-for i, r in enumerate([204, 201, 202, 203]):
-    add_set('M87v2', 6, r, f'M87v2_a1_m6', colors='I_gI')
-for i, r in enumerate([128, 125, 126, 127]):
-    add_set('M87v2', 6, r, f'M87v2_b1_m6', colors='I_gI')
-for i, r in enumerate([44, 41, 42, 43]):
-    add_set('M87v2', 6, r, f'M87v2_c1_m6', colors='I_gI')
+        
+def load_M87v2():
+    print('M87v2')
+    for m in range(1, 6):
+        add_set('M87v2', m, 204, f'M87v2_a1_m{m}', colors='I_gI')
+        add_set('M87v2', m, 128, f'M87v2_b1_m{m}', colors='I_gI')
+        add_set('M87v2', m, 44, f'M87v2_c1_m{m}', colors='I_gI')
+    for i, r in enumerate([204, 201, 202, 203]):
+        add_set('M87v2', 6, r, f'M87v2_a1_m6', colors='I_gI')
+    for i, r in enumerate([128, 125, 126, 127]):
+        add_set('M87v2', 6, r, f'M87v2_b1_m6', colors='I_gI')
+    for i, r in enumerate([44, 41, 42, 43]):
+        add_set('M87v2', 6, r, f'M87v2_c1_m6', colors='I_gI')
 
-print('M49')
-for m in range(1, 6):
-    add_set('M49', m, 204, f'M49_a1_m{m}')
-    add_set('M49', m, 124, f'M49_b1_m{m}')
-    add_set('M49', m, 40, f'M49_c1_m{m}')
-for i, r in enumerate([204, 201, 202, 203]):
-    add_set('M49', 6, r, f'M49_a1_m6')
-for i, r in enumerate([124, 121, 122, 123]):
-    add_set('M49', 6, r, f'M49_b1_m6')
-for i, r in enumerate([40, 37, 38, 39]):
-    add_set('M49', 6, r, f'M49_c1_m6')
-    
-print('NGC3377')
-for m in range(1, 6):
-    add_set('NGC3377', m, 173, f'NGC3377_a1_m{m}')
-    add_set('NGC3377', m, 97, f'NGC3377_b1_m{m}')
-    add_set('NGC3377', m, 41, f'NGC3377_c1_m{m}')
-for i, r in enumerate([173, 174, 175, 176]):
-    add_set('NGC3377', 6, r, f'NGC3377_a1_m6')
-for i, r in enumerate([97, 98, 99, 100]):
-    add_set('NGC3377', 6, r, f'NGC3377_b1_m6')
-for i, r in enumerate([41, 42, 43, 44]):
-    add_set('NGC3377', 6, r, f'NGC3377_c1_m6')
+        
+def load_M49():
+    print('M49')
+    for m in range(1, 6):
+        add_set('M49', m, 204, f'M49_a1_m{m}')
+        add_set('M49', m, 124, f'M49_b1_m{m}')
+        add_set('M49', m, 40, f'M49_c1_m{m}')
+    for i, r in enumerate([204, 201, 202, 203]):
+        add_set('M49', 6, r, f'M49_a1_m6')
+    for i, r in enumerate([124, 121, 122, 123]):
+        add_set('M49', 6, r, f'M49_b1_m6')
+    for i, r in enumerate([40, 37, 38, 39]):
+        add_set('M49', 6, r, f'M49_c1_m6')
 
-print('NGC4993')
-for m in range(1, 6):
-    add_set('NGC4993', m, 203, f'NGC4993_a1_m{m}')
-    add_set('NGC4993', m, 143, f'NGC4993_b1_m{m}')
-    add_set('NGC4993', m, 83, f'NGC4993_c1_m{m}')
-for m in [6]:
-    add_set('NGC4993', m, 203, f'NGC4993_a1_m{m}', model=base_models[5])
-    add_set('NGC4993', m, 143, f'NGC4993_b1_m{m}', model=base_models[5])
-    add_set('NGC4993', m, 83, f'NGC4993_c1_m{m}', model=base_models[5])
-    
-print('M31')
-m31_regions = {
-    'a': 'summer_r1',
-    'b': 'winter_r9',
-    'c': 'winter_r1',
-    'd': 'winter_r2',
-    'e': 'winter_r10'
-}
-m31_rpix = {
-    'a': 6250,
-    'b': 3550,
-    'c': 2050,
-    'd': 1350,
-    'e': 700
-}
-for m in range(1, 7):
-    for i, r in enumerate(['a', 'b', 'c', 'd', 'e']):
-        k = f'M31_{r}_m{m}'
-        add_set_v2('M31', m, r, f'M31_{r}_m{m}', 'm31_'+m31_regions[r])
-        radii_am[k] = m31_rpix[r] * 0.05 / 60.
-        radii_kpc[k] = radii_am[k] * (np.pi/(180.*60.)) * 1e3 * ppy.distancemodels.dmod_to_mpc(dmods['M31'])
+        
+def load_NGC3377():
+    print('NGC3377')
+    for m in range(1, 6):
+        add_set('NGC3377', m, 173, f'NGC3377_a1_m{m}')
+        add_set('NGC3377', m, 97, f'NGC3377_b1_m{m}')
+        add_set('NGC3377', m, 41, f'NGC3377_c1_m{m}')
+    for i, r in enumerate([173, 174, 175, 176]):
+        add_set('NGC3377', 6, r, f'NGC3377_a1_m6')
+    for i, r in enumerate([97, 98, 99, 100]):
+        add_set('NGC3377', 6, r, f'NGC3377_b1_m6')
+    for i, r in enumerate([41, 42, 43, 44]):
+        add_set('NGC3377', 6, r, f'NGC3377_c1_m6')
+    for m in range(7, 8):
+        add_set('NGC3377', m, 173, f'NGC3377_a1_m{m}')
+        add_set('NGC3377', m, 97, f'NGC3377_b1_m{m}')
+        add_set('NGC3377', m, 41, f'NGC3377_c1_m{m}')
 
-print('M51')
-m51_rpix = {
-    'a': 1745,
-    'b': 2776,
-    'c': 2471,
-    'd': 2709,
-    'e': 678
-}
-for m in range(1, 5):
-    model = base_models[m].copy()
-    model.dust_model = ppy.dustmodels.FixedWidthLogNormDust(0.1)
-    for i, r in enumerate(['a', 'b', 'c', 'd', 'e']):
-        k = f'M51_{r}_m{m}'
-        add_set_v2('M51', m, r, f'M51_{r}_m{m}', f'M51_I_BI_{i+1}',
-                   model=model)
-        radii_am[k] = m51_rpix[r] * 0.05 / 60.
-        radii_kpc[k] = radii_am[k] * (np.pi/(180.*60.)) * 1e3 * ppy.distancemodels.dmod_to_mpc(dmods['M51'])
+        
+def load_NGC4993():
+    print('NGC4993')
+    for m in range(1, 6):
+        add_set('NGC4993', m, 203, f'NGC4993_a1_m{m}')
+        add_set('NGC4993', m, 143, f'NGC4993_b1_m{m}')
+        add_set('NGC4993', m, 83, f'NGC4993_c1_m{m}')
+    for m in [6]:
+        add_set('NGC4993', m, 203, f'NGC4993_a1_m{m}', model=base_models[5])
+        add_set('NGC4993', m, 143, f'NGC4993_b1_m{m}', model=base_models[5])
+        add_set('NGC4993', m, 83, f'NGC4993_c1_m{m}', model=base_models[5])
+
+        
+def load_M31():
+    print('M31')
+    m31_regions = {
+        'a': 'summer_r1',
+        'b': 'winter_r9',
+        'c': 'winter_r1',
+        'd': 'winter_r2',
+        'e': 'winter_r10'
+    }
+    m31_rpix = {
+        'a': 6250,
+        'b': 3550,
+        'c': 2050,
+        'd': 1350,
+        'e': 700
+    }
+    for m in range(1, 7):
+        for i, r in enumerate(['a', 'b', 'c', 'd', 'e']):
+            k = f'M31_{r}_m{m}'
+            add_set_v2('M31', m, r, f'M31_{r}_m{m}', 'm31_'+m31_regions[r])
+            radii_am[k] = m31_rpix[r] * 0.05 / 60.
+            radii_kpc[k] = radii_am[k] * (np.pi/(180.*60.)) * 1e3 * ppy.distancemodels.dmod_to_mpc(dmods['M31'])
+
+            
+def load_M51():
+    print('M51')
+    m51_rpix = {
+        'a': 1745,
+        'b': 2776,
+        'c': 2471,
+        'd': 2709,
+        'e': 678
+    }
+    for m in range(1, 4):
+        model = base_models[m].copy()
+        model.dust_model = ppy.dustmodels.FixedWidthLogNormDust(0.1)
+        for i, r in enumerate(['a', 'b', 'c', 'd', 'e']):
+            k = f'M51_{r}_m{m}'
+            add_set_v2('M51', m, r, f'M51_{r}_m{m}', f'M51_I_BI_{i+1}',
+                       model=model)
+            radii_am[k] = m51_rpix[r] * 0.05 / 60.
+            radii_kpc[k] = radii_am[k] * (np.pi/(180.*60.)) * 1e3 * ppy.distancemodels.dmod_to_mpc(dmods['M51'])
+
+
+load_M87()
+load_M87v2()
+load_M49()
+load_NGC3377()
+load_NGC4993()
+load_M31()
+load_M51()
