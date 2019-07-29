5e3748066d7f479ae86cf25dc94a0ed155e36d74
Ben Cook <bcook@cfa.harvard.edu>
2019-07-29 09:17:34 -0400

    Running like_mode 4 jobs

--- paper2/submit_final.sh
+++ paper2/submit_final.sh
@@ -1,38 +1,50 @@
 #!/bin/bash
 
-echo 'Models 20'
-sbatch --array=[204,128,44] slurm/M87v2_m20.slurm
+echo 'Models 28'
+sbatch --array=[204,128,44] slurm/M87v2_m28.slurm
 sleep 1
-sbatch --array=[204,124,40] slurm/M49_m20.slurm
+sbatch --array=[204,124,40] slurm/M49_m28.slurm
 sleep 1
-sbatch --array=[173,97,41] slurm/NGC3377_m20.slurm
+sbatch --array=[173,97,41] slurm/NGC3377_m28.slurm
 sleep 1
-sbatch --array=[16-17] slurm/NGC4993_m20.slurm
+sbatch --array=[16-17] slurm/NGC4993_m28.slurm
 sleep 1
-sbatch --array=[1-5] slurm/M31_m20.slurm
-sleep 1
-
-echo 'Models 21'
-sbatch --array=[204,128,44] slurm/M87v2_m21.slurm
-sleep 1
-sbatch --array=[204,124,40] slurm/M49_m21.slurm
-sleep 1
-sbatch --array=[173,97,41] slurm/NGC3377_m21.slurm
-sleep 1
-sbatch --array=[16-17] slurm/NGC4993_m21.slurm
-sleep 1
-sbatch --array=[1-5] slurm/M31_m21.slurm
+sbatch --array=[1-5] slurm/M31_m28.slurm
 sleep 1
 
-echo 'Models 22'
-sbatch --array=[204,128,44] slurm/M87v2_m22.slurm
+echo 'Models 29'
+sbatch --array=[204,128,44] slurm/M87v2_m29.slurm
 sleep 1
-sbatch --array=[204,124,40] slurm/M49_m22.slurm
+sbatch --array=[204,124,40] slurm/M49_m29.slurm
 sleep 1
-sbatch --array=[173,97,41] slurm/NGC3377_m22.slurm
+sbatch --array=[173,97,41] slurm/NGC3377_m29.slurm
 sleep 1
-sbatch --array=[16-17] slurm/NGC4993_m22.slurm
+sbatch --array=[16-17] slurm/NGC4993_m29.slurm
 sleep 1
-sbatch --array=[1-5] slurm/M31_m22.slurm
+sbatch --array=[1-5] slurm/M31_m29.slurm
+sleep 1
+
+echo 'Models 30'
+sbatch --array=[204,128,44] slurm/M87v2_m30.slurm
+sleep 1
+sbatch --array=[204,124,40] slurm/M49_m30.slurm
+sleep 1
+sbatch --array=[173,97,41] slurm/NGC3377_m30.slurm
+sleep 1
+sbatch --array=[16-17] slurm/NGC4993_m30.slurm
+sleep 1
+sbatch --array=[1-5] slurm/M31_m30.slurm
+sleep 1
+
+echo 'Models 31'
+sbatch --array=[204,128,44] slurm/M87v2_m31.slurm
+sleep 1
+sbatch --array=[204,124,40] slurm/M49_m31.slurm
+sleep 1
+sbatch --array=[173,97,41] slurm/NGC3377_m31.slurm
+sleep 1
+sbatch --array=[16-17] slurm/NGC4993_m31.slurm
+sleep 1
+sbatch --array=[1-5] slurm/M31_m31.slurm
 sleep 1
 
